c318db2f220c91c931de5de3721a6ea3b7d39887
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-19 15:38:16 +0300

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', Throttle(onPlay, 1000));
+
+function onPlay(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import Throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', Throttle(handleFormInput), 500);
+
+function handleFormInput(event) {
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  const form = {
+    email,
+    message,
+  };
+  console.log(localStorage.setItem('feedback-form-state', form));
+}
+
+formRef.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  const form = {
+    email,
+    message,
+  };
+  console.log(form);
+  event.currentTarget.reset();
+}

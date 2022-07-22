fe3b7d6c9036db881eb0ec13614bf92ba0f46f0c
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-22 14:57:43 +0300

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,22 @@ import Throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', Throttle(onPlay, 1000));
 
 function onPlay(event) {
-  localStorage.setItem('videoplayer-current-time', event.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, event.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+setCurrentTime();
+
+function setCurrentTime() {
+  const setTime = localStorage.getItem(CURRENT_TIME_KEY);
+  if (setTime) {
+    player.setCurrentTime(setTime);
+  }
+}
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
